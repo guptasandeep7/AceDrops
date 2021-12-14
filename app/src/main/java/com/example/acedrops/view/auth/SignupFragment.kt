ab39544cbb03edee9787502abe02d7c64d7422ee
Sandeep Gupta <guptasg300@gmail.com>
2021-12-14 20:13:00 +0530

    Development branch Created

---
+++ app/src/main/java/com/example/acedrops/view/auth/SignupFragment.kt
@@ -0,0 +1,34 @@
+package com.example.acedrops.view.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.acedrops.R
+import com.example.acedrops.viewmodel.AuthViewModel
+
+class SignupFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignupFragment()
+    }
+
+    lateinit var authViewModel: AuthViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+
+    }
+}
