7d08ee133531ff7ce2436ee0ce05e3ef76d624af
Sandeep Gupta <guptasg300@gmail.com>
2021-12-15 12:18:07 +0530

    Packages created and Dependencies added

--- app/src/main/java/com/example/acedrops/view/auth/AuthActivity.kt
+++ app/src/main/java/com/example/acedrops/view/auth/AuthActivity.kt
@@ -8,6 +8,7 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        supportActionBar?.hide()
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, LoginFragment.newInstance())
